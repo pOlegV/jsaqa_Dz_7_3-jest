3645760b2611a2fb41d42341ac803e36f1d8f607
pOlegV <oleg-1@yandex.ru>
2023-07-18 14:00:04 +0300

    Finish commit

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const config = {
+  collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/coverage/**",
+    "!jest.config.js",
+  ],
+  coverageThreshold: {
+    branches: 100,
+    functions: 100,
+    lines: 100,
+  },
+};
+
+module.exports = config;
--- test/unit/sortByName.test.js
+++ test/unit/sortByName.test.js
@@ -2,16 +2,26 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    const result = sorting.sortByName(input);
+    expect(result).toEqual(expected);
+
+    const input2 = ["Властелин Колец", "Властелин Колец"];
+
+    const expected2 = ["Властелин Колец", "Властелин Колец"];
+
+    const result2 = sorting.sortByName(input2);
+    expect(result2).toEqual(expected2);
   });
 });
